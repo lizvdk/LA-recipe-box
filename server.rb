4625fe2b7d865eb161d7ba38753e7c9847b8da12
Liz Vanderkloot <liz.m.vdk@gmail.com>
2014-11-29 08:53:30 -0500

    Recipe names display as link on /recipes

--- server.rb
+++ server.rb
@@ -12,12 +12,21 @@ def db_connection
 end
 
 get '/recipes' do
+  query=
+  "SELECT
+    recipes.name as recipe_name,
+    recipes.id as recipe_id
+  FROM
+    recipes;"
+  @recipes = db_connection{|db|
+    db.exec(query)}
+
   #lists the names of all the recipes in the database, sorted a-z
   #each recipe name links to the recipes details page
   erb :'recipes/index'
 end
 
-get '/recipes/:id'
+get '/recipes/:id' do
   #show the details for a recipe with the given ID.
   #includes recipe name, description, and instructions.
   #lists the ingredients required for the recipe.
