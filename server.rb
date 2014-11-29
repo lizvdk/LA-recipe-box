d8af4013dda6416f5b8e798c7b7e83fb49d5b6aa
Liz Vanderkloot <liz.m.vdk@gmail.com>
2014-11-29 08:41:14 -0500

    Requirements as comments

---
+++ server.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pg'
+
+def db_connection
+  begin
+    connection = PG.connect(dbname: 'recipes')
+    yield(connection)
+  ensure
+    connection.close
+  end
+end
+
+get '/recipes' do
+  #lists the names of all the recipes in the database, sorted a-z
+  #each recipe name links to the recipes details page
+end
+
+get '/recipes/:id'
+  #show the details for a recipe with the given ID.
+  #includes recipe name, description, and instructions.
+  #lists the ingredients required for the recipe.
+end
